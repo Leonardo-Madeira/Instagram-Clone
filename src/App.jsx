65efc519f2470ecb126a703e8fa238827761e213
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-24 21:19:51 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {db} from './firebase.js';
 import {useEffect , useState} from 'react'
 
+import './App.scss'
+
 export function App() {
 
   const [user, setUser] = useState(null);
@@ -11,7 +13,19 @@ export function App() {
 
   return(
     <div className="app">
-      <h1>Hello, world!</h1>
+      <div className="header">
+        <div className="headerLogo">
+          <a href=""><img src="" alt="" /></a>
+        </div>
+
+        <div className="headerloginForm">
+          <form action="">
+            <input type="text" placeholder="Login" />
+            <input type="password" placeholder="Senha" name="" id="" />
+            <input type="submit" name="acao"  value="Logar!" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
